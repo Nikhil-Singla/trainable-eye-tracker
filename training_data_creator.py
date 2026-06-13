3c9139c7e2ed238f0627c3482fbce6c186ded4e9
Nikhil Singla <nsingla3.14@gmail.com>
2026-06-13 15:46:56 -0700

    [002] Basic labelling with xy coordinates

--- training_data_creator.py
+++ training_data_creator.py
@@ -1,4 +1,7 @@
 import cv2
+import pyautogui
+
+# Get current coordinates
 
 # Initialize the default Mac camera (0 is usually the built-in FaceTime HD camera)
 cap = cv2.VideoCapture(0)
@@ -20,9 +23,19 @@ while True:
     # Display the resulting video frame
     cv2.imshow('Mac Camera Feed', frame)
 
-    # Stop the loop when the 'q' key is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    # Read a single keypress per loop iteration
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
         break
+
+    if key == ord('s'):
+        # Save the current frame as an image file
+        x, y = pyautogui.position()
+        print(f"X: {x}, Y: {y}")
+        filename = f'captured_frame-{x}-{y}.jpg'
+
+        cv2.imwrite(filename, frame)
+        print(f"Frame captured and saved as '{filename}'.")
 
 # Release the camera hardware resource and destroy all active windows
 cap.release()
