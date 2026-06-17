d7bc61c869f371c799d0e19b3560493720ecef88
Nikhil Singla <nsingla3.14@gmail.com>
2026-06-17 06:58:34 -0700

    [003-NEEDS TESTING] Camera check, filename randomize, data variety. Finished MVP for raw image collection

--- training_data_creator.py
+++ training_data_creator.py
@@ -1,14 +1,19 @@
 import cv2
 import pyautogui
+import random
 
 # Get current coordinates
+# Initialize the default camera (0 is usually the built-in FaceTime HD camera for Mac)
+available = False
 
-# Initialize the default Mac camera (0 is usually the built-in FaceTime HD camera)
-cap = cv2.VideoCapture(0)
-
-if not cap.isOpened():
-    print("Error: Could not open the camera.")
-    exit()
+for i in range(10):
+    cap = cv2.VideoCapture(i)
+    if cap.isOpened():
+        available = True
+        break
+    
+if not available:
+    raise ValueError("Error: Could not open the camera.")
 
 print("Press 'q' to close the camera window.")
 
@@ -26,13 +31,25 @@ while True:
     # Read a single keypress per loop iteration
     key = cv2.waitKey(1) & 0xFF
     if key == ord('q'):
+        # Quit app
         break
 
-    if key == ord('s'):
+    if key == ord('c'):
         # Save the current frame as an image file
         x, y = pyautogui.position()
+        
+        seed = random.randint(10**9)
+
         print(f"X: {x}, Y: {y}")
-        filename = f'captured_frame-{x}-{y}.jpg'
+        filename = f'{seed}-{x}-{y}.jpg'
+
+        cv2.imwrite(filename, frame)
+        print(f"Frame captured and saved as '{filename}'.")
+
+    if key == ord('b'):
+        # Save samples when not looking at the screen.
+        seed = random.randint(10**9)
+        filename = f'{seed}-OUTSIDE.jpg'
 
         cv2.imwrite(filename, frame)
         print(f"Frame captured and saved as '{filename}'.")
